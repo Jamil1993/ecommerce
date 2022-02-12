755ec7bb3033f8a4219007cafc5a232f3c7d787c
Jamil1993 <jamilfozilovmajidovich@gmail.com>
2022-02-12 21:58:55 +0500

    End of section 13 API - Basket

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using StackExchange.Redis;
 
 namespace API
 {
@@ -28,6 +28,11 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x=>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            
+            services.AddSingleton<IConnectionMultiplexer>(c => {
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt => 
